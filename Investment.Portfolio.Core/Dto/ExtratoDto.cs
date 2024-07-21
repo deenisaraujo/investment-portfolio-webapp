ded7f8ff38fa22b17138a7776bd37f84d4671a3f
Dênis Deusdeth de Araújo <denis.araujo@7comm.com.br>
2024-07-21 17:36:09 -0300

    implementando a funcionalidade de extrato

--- Investment.Portfolio.Core/Dto/ExtratoDto.cs
+++ Investment.Portfolio.Core/Dto/ExtratoDto.cs
@@ -1,27 +1,26 @@
-﻿using Investment.Portfolio.Core.Model;
+﻿using Investment.Portfolio.Core.Enum;
+using Investment.Portfolio.Core.Model;
 
 namespace Investment.Portfolio.Core.Dto
 {
     public class ExtratoDto
     {
         public int NR_NOTA { get; set; }
-        public DateTime DT_OPERACAO { get; set; }
         public long NR_CPF_CNPJ { get; set; }
         public string DS_ATIVO { get; set; }
         public string DS_TIPO_PRODUTO { get; set; }
         public string DS_NEGOCIACAO { get; set; }
-        public char DS_TIPO_OPERACAO { get; set; }
+        public TipoOperacaoEnum DS_TIPO_OPERACAO { get; set; }
         public string DS_ESPECIFICACAO_TITULO { get; set; }
         public int NR_QUANTIDADE { get; set; }
         public decimal VL_PRECO { get; set; }
         public decimal VL_OPERACAO { get; set; }
-        public DateTime DT_ALTERACAO { get; set; }
+        public DateTime DT_OPERACAO { get; set; }
 
         public ExtratoModel toModel() =>
              new()
              {
                  Nota = NR_NOTA,
-                 DataOperacao = DT_OPERACAO,
                  CpfCnpj = NR_CPF_CNPJ,
                  Ativo = DS_ATIVO,
                  TipoProduto = DS_TIPO_PRODUTO,
@@ -31,7 +30,7 @@ namespace Investment.Portfolio.Core.Dto
                  Quantidade = NR_QUANTIDADE,
                  Preco = VL_PRECO,
                  ValorOperacao = VL_OPERACAO,
-                 DataAlteracao = DT_ALTERACAO
+                 DataOperacao = DT_OPERACAO
              };
     }
 }
--- Investment.Portfolio.Core/Repository/OperacoesCompraVenda/OperacoesCompraVendaRepository.cs
+++ Investment.Portfolio.Core/Repository/OperacoesCompraVenda/OperacoesCompraVendaRepository.cs
@@ -6,13 +6,14 @@ using Dapper;
 using Investment.Portfolio.Core.Dto;
 using System.Net;
 using Investment.Portfolio.Core.Request;
+using Org.BouncyCastle.Asn1.Ocsp;
 
 namespace Investment.Portfolio.Core.Repository.OperacoesCompraVenda
 {
     public class OperacoesCompraVendaRepository : IOperacoesCompraVendaRepository
     {
         private readonly string QueryCompraVenda = "INSERT INTO invest_portf.TB_ORDEM(ID_PRODUTO,NR_CPF_CNPJ,DS_ATIVO,DS_TIPO_PRODUTO,DS_NEGOCIACAO,DS_TIPO_OPERACAO,DS_ESPECIFICACAO_TITULO,NR_QUANTIDADE,VL_PRECO,VL_OPERACAO,DT_OPERACAO)";
-        private readonly string QueryExtrato = "";
+        private readonly string QueryExtrato = "SELECT * FROM invest_portf.TB_ORDEM";
         private readonly IConnectionFactory _factory;
         public OperacoesCompraVendaRepository(IConnectionFactory factory)
         {
@@ -28,7 +29,7 @@ namespace Investment.Portfolio.Core.Repository.OperacoesCompraVenda
                     using (var cmd = conn.CreateCommand())
                     {
                         conn.Open();
-                        cmd.CommandText = QueryCompraVenda + $" VALUES({produto.IdProduto},{ordem.CpfCnpj},'{produto.Ativo}', '{produto.TipoProduto}', '{produto.Negociacao}', '{ordem.TipoOperacao}', '{produto.EspecificacaoTitulo}', {produto.QuantidadeDisponivel}, {produto.Preco.ToString().Replace(",", ".")}, {(produto.Preco * ordem.Quantidade).ToString().Replace(",", ".")}, '{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}')";
+                        cmd.CommandText = QueryCompraVenda + $" VALUES({produto.IdProduto},{ordem.CpfCnpj},'{produto.Ativo}', '{produto.TipoProduto}', '{produto.Negociacao}', '{ordem.TipoOperacao}', '{produto.EspecificacaoTitulo}', {ordem.Quantidade}, {produto.Preco.ToString().Replace(",", ".")}, {(produto.Preco * ordem.Quantidade).ToString().Replace(",", ".")}, '{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}')";
                         await conn.ExecuteAsync(cmd.CommandText);
                     }
                 }
@@ -49,7 +50,10 @@ namespace Investment.Portfolio.Core.Repository.OperacoesCompraVenda
                     using (var cmd = conn.CreateCommand())
                     {
                         conn.Open();
-                        cmd.CommandText = QueryExtrato;
+                        if (!dataExtrato.Equals(null) && dataExtrato.ToString() != "01/01/0001 00:00:00")
+                            cmd.CommandText = QueryExtrato + $" WHERE DT_OPERACAO = '{dataExtrato.ToString("yyyy-MM-dd")}'";
+                        else
+                            cmd.CommandText = QueryExtrato ;
                         var result = await conn.QueryAsync<ExtratoDto>(cmd.CommandText);
                         return result.Select(x => x.toModel());
                     }
